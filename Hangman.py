24f7b4a7398c6e547202ad1d3101525a1b04a857
Brandon Eng <brandoneng000@gmail.com>
2017-03-20 14:32:55 -0700

    Started Drawing Hangman

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,82 @@
-print("hello")
+
+
+def drawHead(state):
+    if(state == 0):
+        print("    _________")
+        print("    |       |")
+        print("    |       |")
+        print("    |       |")
+        print("    |      ")
+        print("    |      ")
+        print("    |      ")
+        print("    |      ")
+    else:
+        print("    _________")
+        print("    |       |")
+        print("    |       |")
+        print("    |      _|_")
+        print("    |     /   \\")
+        print("    |    |     |")
+        print("    |     \___/")
+        print("    |       |  ")
+
+def drawBodyAndArms(state):
+    if(state == 0):
+        print("    |     ")
+        print("    |     ")
+        print("    |     ")
+        print("    |     ")
+        print("    |     ")
+        print("    |     ")
+    elif(state == 1):
+        print("    |       |  ")
+        print("    |       |")
+        print("    |       |")
+        print("    |       |")
+        print("    |       |")
+        print("    |       |")
+    elif(state == 2):
+        print("    |       | ")
+        print("    |      /| ")
+        print("    |     / | ")
+        print("    |    /  | ")
+        print("    |       |")
+        print("    |       |")
+    elif(state == 3):
+        print("    |       |  ")
+        print("    |      /|\\")
+        print("    |     / | \\")
+        print("    |    /  |  \\")
+        print("    |       |")
+        print("    |       |")
+
+def drawLegs(state):
+    if (state == 0):
+        print("    |   ")
+        print("    |   ")
+        print("    |   ")
+        print("    |   ")
+        print("    |")
+        print("    |")
+        print("____|____")
+    elif (state == 1):
+        print("    |      / ")
+        print("    |     /  ")
+        print("    |    /   ")
+        print("    |   /    ")
+        print("    |")
+        print("    |")
+        print("____|____")
+    elif (state == 2):
+        print("    |      / \\")
+        print("    |     /   \\")
+        print("    |    /     \\")
+        print("    |   /       \\")
+        print("    |")
+        print("    |")
+        print("____|____")
+
+        
+drawHead(1)
+drawBodyAndArms(3)
+drawLegs(2)
