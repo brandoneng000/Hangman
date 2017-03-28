d82e32ba6b84b7f2b9add9c1d4805523ff836a27
Brandon Eng <brandoneng000@gmail.com>
2017-03-28 16:34:36 -0700

    Completed basic hangman

--- Hangman.py
+++ Hangman.py
@@ -6,6 +6,7 @@ file = open("Dictionary.txt", "r+")
 word = ""
 mystery = ""
 
+
 def draw_head(state):
     if state == 0:
         print("    _________")
@@ -85,17 +86,18 @@ def draw_legs(state):
         print("____|____")
 
 
-def hide_words(mystery):
-    for letter in range(0, len(mystery)):
-        if mystery[letter].isalpha():
-            mystery = mystery[:letter] + "_" + mystery[letter + 1:]
+def hide_words(myst):
+    for letter in range(0, len(myst)):
+        if myst[letter].isalpha():
+            myst = myst[:letter] + "_" + myst[letter + 1:]
         letter += 1
 
-    return mystery
+    return myst
 
 
 def draw_hangman():
     global attempts
+    game_state = 1
     if attempts == 0:
         draw_head(0)
         draw_body_and_arms(0)
@@ -115,22 +117,25 @@ def draw_hangman():
             draw_legs(1)
         elif attempts == 6:
             draw_legs(2)
+            game_state = 0
         else:
             draw_legs(0)
 
+    return game_state
 
-def letter_check(mystery, word, char):
+
+def letter_check(myst, origin, char):
     global attempts
     counter = 0
-    for letter in range(0, len(mystery)):
-        if word[letter] == char or word[letter] == char.upper():
-            mystery = mystery[:letter] + char + mystery[letter + 1:]
+    for letter in range(0, len(myst)):
+        if origin[letter] == char or origin[letter] == char.upper():
+            myst = myst[:letter] + char + myst[letter + 1:]
             counter += 1
 
     if counter == 0:
         attempts += 1
 
-    return mystery
+    return myst
 
 
 def get_guess():
@@ -147,22 +152,29 @@ def initialize_game():
     global word
 
     lines = file.readlines()
-    word = random.choice(lines)[:-1]
-    word = lines[0][:-1]
-    draw_hangman()
+    word = random.choice(lines)[:-1].lower()
+    word = lines[0][:-1].lower()
     mystery = hide_words(word)
     print("Guess what the word is?")
-    print(mystery);
+    print(mystery)
 
 
 def main():
-    initialize_game()
     global mystery
     global word
+    lose = draw_hangman()
 
-    guess = get_guess()
-    mystery = letter_check(mystery, word, guess)
-    print("Current status is %s", mystery)
+    initialize_game()
+    while mystery != word and lose == 1:
+        guess = get_guess()
+        mystery = letter_check(mystery, word, guess)
+        print("Current status is %s" % mystery)
+        lose = draw_hangman()
+
+    if mystery == word:
+        print("Congratulations!!! You Win!!!")
+    elif lose == 0:
+        print("You Lose!!! The word was", word)
 
 
 if __name__ == "__main__":
